69b878bcfc90a2f9a663be1f45d09b1be42f18b2
n4mlz <n4mlz.dev@gmail.com>
2024-08-14 16:11:21 +0900

    update: step 8

--- src/kernel.c
+++ src/kernel.c
@@ -29,8 +29,8 @@ struct sbiret sbi_call(long arg0, long arg1, long arg2, long arg3, long arg4,
 void kernel_main(void) {
   memset(__bss, 0, (size_t)__bss_end - (size_t)__bss);
 
-  printf("\n\nHello %s\n", "World!");
-  printf("1 + 2 = %d, %x\n", 1 + 2, 0x1234abcd);
+  PANIC("booted!");
+  printf("unreachable here!\n");
 
   for (;;)
     __asm__ __volatile__("wfi");
