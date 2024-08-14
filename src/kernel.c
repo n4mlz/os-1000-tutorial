701736c24a8df402269f97c12ed19eee60932e00
n4mlz <n4mlz.dev@gmail.com>
2024-08-14 16:02:27 +0900

    update: step 7

--- src/kernel.c
+++ src/kernel.c
@@ -26,17 +26,6 @@ struct sbiret sbi_call(long arg0, long arg1, long arg2, long arg3, long arg4,
   return (struct sbiret){.error = a0, .value = a1};
 }
 
-void putchar(char ch) {
-  sbi_call(ch, 0, 0, 0, 0, 0, 0, 1 /* Console Putchar */);
-}
-
-void *memset(void *buf, char c, size_t n) {
-  uint8_t *p = (uint8_t *)buf;
-  while (n--)
-    *p++ = c;
-  return buf;
-}
-
 void kernel_main(void) {
   memset(__bss, 0, (size_t)__bss_end - (size_t)__bss);
 
